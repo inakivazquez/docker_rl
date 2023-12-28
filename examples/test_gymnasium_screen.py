b05cc711a931e337838044f5719d6895ad070804
Inaki Vazquez <ivazquez@deusto.es>
2023-12-28 23:10:16 +0100

    Standalone docker and arg parsing

--- examples/test_gymnasium_screen.py
+++ examples/test_gymnasium_screen.py
@@ -1,11 +1,22 @@
 import gymnasium as gym
-from gymnasium.wrappers import RecordVideo
+import sys
 
+if len(sys.argv) < 2:
+	print(f"Use: {sys.argv[0]} env [n_steps]")
+	print(f"Example: {sys.argv[0]} CartPole-v1")
+	print(f"Example: {sys.argv[0]} CartPole-v1 200000")
+	quit()
 
-env = gym.make("CartPole-v1", render_mode="human")
+str_env = sys.argv[1]
+if len(sys.argv) > 2:
+	n_steps = int(sys.argv[2])
+else:
+	n_steps = 1_000
+
+env = gym.make(str_env, render_mode="human")
 
 observation, info = env.reset(seed=42)
-for step_index in range(1000):
+for step_index in range(n_steps):
 	action = env.action_space.sample()
 	observation, reward, terminated, truncated, info = env.step(action)
 
