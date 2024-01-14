70f8189a5c5a176d02d890b9058d9aedcb36e442
Inaki Vazquez <ivazquez@deusto.es>
2024-01-14 10:58:11 +0100

    Updated examples header information

--- examples/check_cuda.py
+++ examples/check_cuda.py
@@ -1,4 +1,21 @@
-# imports are always needed
+#!/usr/bin/env python
+""" Checks it CUDA is available.
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+__author__ = "Inaki Vazquez"
+__email__ = "ivazquez@deusto.es"
+__license__ = "GPLv3"
+
 import torch
 
 n_gpus = torch.cuda.device_count()
--- examples/test_gymnasium.py
+++ examples/test_gymnasium.py
@@ -1,3 +1,21 @@
+#!/usr/bin/env python
+""" Basic Gymnasium environments tester.
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+__author__ = "Inaki Vazquez"
+__email__ = "ivazquez@deusto.es"
+__license__ = "GPLv3"
+
 import gymnasium as gym
 from gymnasium.wrappers.record_video import RecordVideo
 import argparse
